49ae1724db4b3db2470a80cf4b54c359fab236f5
DanMagor <anybis-maat@mail.ru>
2018-10-20 20:35:12 +0300

    AttackCardGenerator Added

---
+++ attack_card_generator.py
@@ -0,0 +1,6 @@
+class AttackCard:
+    damage = 0
+    bullets = 0
+    accuracy_penalty = 0
+
+
---
+++ card_generator.py
@@ -0,0 +1,4 @@
+
+damage_range = range(0, 25)
+bullets_range = range(0, 6)
+accuracy_penalty_range = range(0, 90, 10)
