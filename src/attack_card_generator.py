d2b84198681349788cf6fd326a3c8268f99de2c7
DanMagor <anybis-maat@mail.ru>
2018-10-20 21:09:13 +0300

    base logic for attack card

--- src/attack_card_generator.py
+++ src/attack_card_generator.py
@@ -1,6 +1,34 @@
+
+
+import random
+
+
+damage_range = range(0, 25)
+bullets_range = range(0, 6)
+accuracy_penalty_range = range(0, 90, 10)
+
+
 class AttackCard:
     damage = 0
     bullets = 0
     accuracy_penalty = 0
+    # TODO: Add Initiative + additional effects fields
+
+    def __init__(self, damage, bullets, accuracy_penalty):
+        """Constructor"""
+        self.damage = damage
+        self.bullets = bullets
+        self.accuracy_penalty = accuracy_penalty
 
 
+def getAttackCardsStack(n):
+    attackCards = []
+    for i in range(0, n):
+        generateAttackCard()
+
+
+def generateAttackCard():
+    damage = random.randint(damage_range.start, damage_range.stop)
+    print(damage)
+
+getAttackCardsStack(10)
--- src/card_generator.py
+++ src/card_generator.py
@@ -1,4 +1,5 @@
 
-damage_range = range(0, 25)
-bullets_range = range(0, 6)
-accuracy_penalty_range = range(0, 90, 10)
+
+
+
+
