c0ec290f9c4c141bf059b11e2ae5574e2cacf7b3
DanMagor <anybis-maat@mail.ru>
2018-10-21 13:02:47 +0300

    attack cards done

--- src/attack_card_generator.py
+++ src/attack_card_generator.py
@@ -1,23 +1,27 @@
 import random
 
-parameters = {'DAMAGE', 'BULLETS', 'ACCURACY_PENALTY'}
+parameters = {'DAMAGE': 0, 'BULLETS': 0, 'ACCURACY_PENALTY': 0}
 
 damage_range_min = 0
-damage_range_max = 25
-bullets_range_min = 0
+damage_range_max = 15
+bullets_range_min = 1
 bullets_range_max = 6
 accuracy_penalty_range_min = 0
 accuracy_penalty_range_max = 90
 accuracy_penalty_range_step = 10
 card_cost = 0
 
+minimal_damage = 8
+
+
 class AttackCard:
     damage = 0
     bullets = 0
     accuracy_penalty = 0
+
     # TODO: Add Initiative + additional effects fields
 
-    def __init__(self, damage, bullets, accuracy_penalty):
+    def __init__(self, damage=0, bullets=0, accuracy_penalty=0):
         """Constructor"""
         self.damage = damage
         self.bullets = bullets
@@ -25,25 +29,68 @@ class AttackCard:
 
 
 def getAttackCardsStack(n):
-    attackCards = []
+    attack_cards_stack = []
     for i in range(0, n):
-        generateAttackCard()
+        card = generateAttackCard()
+        while calculateCardFitness(card) < minimal_damage:
+            card = generateAttackCard()
+        attack_cards_stack.append(card)
+    return attack_cards_stack
 
-def normalization():
-    damage = (random.randint(damage_range_min, damage_range_max)  - damage_range_min) / \
+
+def calculateCardFitness(card):
+    #TODO: Add2 patameters
+    fitness = card.bullets*(card.damage*(100-card.accuracy_penalty)/100)
+    return fitness
+
+def generateAttackCard():
+    damage = (random.randint(damage_range_min, damage_range_max) - damage_range_min) / \
              (damage_range_max - damage_range_min)
     bullets = (random.randint(bullets_range_min, bullets_range_max) - bullets_range_min) / \
               (bullets_range_max - bullets_range_min)
-    accuracy_penalty = (random.randint(accuracy_penalty_range_min, accuracy_penalty_range_max, accuracy_penalty_range_step)
+    accuracy_penalty = (random.randrange(accuracy_penalty_range_min, accuracy_penalty_range_max,
+                                         accuracy_penalty_range_step)
                         - accuracy_penalty_range_min) / (accuracy_penalty_range_max - accuracy_penalty_range_min)
 
+    # TODO: Add 2 other parameters
 
-def generateAttackCard():
-    card_cost = 5 * random.choice(list(parameters))
-    while (len(parameters)== 0):
-        rand = random.choice(list(parameters))
-        coefficient = 10 - card_cost / 5.55
-        dict.pop(rand)
-        card_cost += coefficient * rand
+    # write random generated and normalized parameters
+    parameters["DAMAGE"] = damage
+    parameters["BULLETS"] = bullets
+    parameters["ACCURACY_PENALTY"] = accuracy_penalty
+
+    parameters_copy = parameters.copy()
+    rand_parameter = random.choice(list(parameters_copy.keys()))
+    k1 = 1 * parameters_copy[rand_parameter]
+    card_cost = k1
+    parameters[rand_parameter] = k1
+    parameters_copy.pop(rand_parameter)
+
+    while parameters_copy:
+        rand_parameter = random.choice(list(parameters_copy.keys()))
+        c = 1 - card_cost / 3
+        k = c * parameters_copy[rand_parameter]
+        card_cost += k
+        parameters[rand_parameter] = k
+        parameters_copy.pop(rand_parameter)
+
+    damage = round(parameters["DAMAGE"] * (damage_range_max - damage_range_min) + damage_range_min)
+    bullets = round(parameters["BULLETS"] * (bullets_range_max - damage_range_min) + damage_range_min)
+    accuracy_penalty = round(parameters["ACCURACY_PENALTY"] * (accuracy_penalty_range_max
+                                                               - accuracy_penalty_range_min) +
+                             accuracy_penalty_range_min, -1)
+    accuracy_penalty = 100 - accuracy_penalty
+    if accuracy_penalty < 0:
+        accuracy_penalty = 0
+
+    if bullets == 0:
+        bullets = 1
+
+    # TODO ADD 2 parameters
+
+    card = AttackCard(damage=damage, bullets=bullets, accuracy_penalty=accuracy_penalty)
+
+    return card
+
 
 getAttackCardsStack(10)
--- src/card_generator.py
+++ src/card_generator.py
@@ -1,5 +1,21 @@
+from attack_card_generator import getAttackCardsStack
+import json
 
+attack_stack = getAttackCardsStack(20)
 
-
-
-
+index = 1
+tabs_number = 1
+js = '{ "Cards" : { '
+for card in attack_stack:
+    temp = '"Card' + str(index) + '" : '
+    js += "" + temp + "{"
+    js += '"Damage" : "' + str(card.damage) + '",'
+    js += '"Bullets" : "' + str(card.bullets) + '",'
+    js += '"Accuracy_Penalty" : "' + str(card.accuracy_penalty) + '"'
+    js += "},"
+    index += 1
+js += '"None" : "None"'
+js += '}}'
+parsed = json.loads(js)
+with open('Cards.json', 'w') as outfile:
+    json.dump(parsed, outfile)
